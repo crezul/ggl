1168f758fc8fab58066c575531e9355b28041a9a
crezul <zammerr@gmail.com>
2023-12-10 21:45:59 +0200

    0.3.1 Minor changes

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
 #!/bin/bash -xe
 
-sudo cp ../build/src/ggl /usr/local/bin/
+if [ "$(uname)" == "Darwin" ]; then
+	sudo cp ../build/src/ggl /usr/local/bin/
+	exit 0
+else
+	echo "Unknown OS: $(uname)"
+	echo "Exiting..."
+	exit 1
+fi
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,18 @@
-#include "lib.hpp"
 #include <iostream>
+#include "lib.hpp"
 using namespace ggl;
 
 void PrintHead() {
-  std::cout << "ggl - google search, version 0.2" << std::endl;
+  std::cout << "ggl - google search, version 0.3.1" << std::endl;
 }
 
 int main(int argc, char *argv[]) {
-
+  PrintHead();
   std::string arguments = SplitAllArguments(argc, argv);
   arguments = EncodeRequest(arguments);
   std::string browser = ggl::GetBrowser(argc, argv);
   std::string searching = "https://www.google.com/search?q=" + arguments;
-  std::string command = "open -a \"" + browser + "\" -u '" +      searching + "'";
+  std::string command = "open -a \"" + browser + "\" -u '" + searching + "'";
   system(command.c_str());
   return 0;
 }
